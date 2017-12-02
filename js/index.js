31d8f1fc498bbfc8722e4f97e892c23d8bb60ad1
fuli <2212163233@qq.com>
2017-12-02 16:29:51 +0800

    首页部分内容

--- js/index.js
+++ js/index.js
@@ -29,5 +29,58 @@ require(["config"], function(){
 			$(".gou-main").html(html);
 		});
 		
+		// 渲染 展示-香水 模板
+		$.ajax("/mock/zhanshi_xs.json").done(function(responseData){			
+			var html = template("zhanshi_xs_template", {products : responseData.data});
+			$(".li-1").html(html);
+		});
+		
+		// 渲染 展示-美妆 模板
+		$.ajax("/mock/zhanshi_mz.json").done(function(responseData){			
+			var html = template("zhanshi_xs_template", {products : responseData.data});
+			$(".li-2").html(html);
+		});
+		
+		// 渲染 展示-家电 模板
+		$.ajax("/mock/zhanshi_jd.json").done(function(responseData){			
+			var html = template("zhanshi_xs_template", {products : responseData.data});
+			$(".li-3").html(html);
+		});
+		
+		// 渲染 展示-家居 模板
+		$.ajax("/mock/zhanshi_jj.json").done(function(responseData){			
+			var html = template("zhanshi_xs_template", {products : responseData.data});
+			$(".li-4").html(html);
+		});
+		
+		// 渲染 展示-鞋包 模板
+		$.ajax("/mock/zhanshi_xb.json").done(function(responseData){			
+			var html = template("zhanshi_xs_template", {products : responseData.data});
+			$(".li-5").html(html);
+		});
+		
+		// 渲染 展示-食品 模板
+		$.ajax("/mock/zhanshi_sp.json").done(function(responseData){			
+			var html = template("zhanshi_xs_template", {products : responseData.data});
+			$(".li-6").html(html);
+		});
+		
+		// 渲染 楼层-香水 模板
+		$.ajax("/mock/floor_xs.json").done(function(responseData){			
+			var html = template("floor_xs_template", {products : responseData.data});
+			$(".fx_main_middle").html(html);
+		});
+		
+		// 渲染 楼层-美容 模板
+		$.ajax("/mock/floor_mr.json").done(function(responseData){			
+			var html = template("floor_mr_template", {products : responseData.data});
+			$(".fm_main_middle").html(html);
+		});
+		
+		// 渲染 楼层-家用电器 模板
+		$.ajax("/mock/floor_jy.json").done(function(responseData){			
+			var html = template("floor_jy_template", {products : responseData.data});
+			$(".fjy_main_middle").html(html);
+		});
 	});
 });
